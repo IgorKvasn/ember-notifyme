60ded97ef99f75692e26ca989a1b587b60688c93
Igor Kvasnička <igor.kvasnicka@annotation.sk>
2016-03-12 18:30:00 +0100

    dependency fix

--- blueprints/ember-notifyme/index.js
+++ blueprints/ember-notifyme/index.js
@@ -5,8 +5,6 @@ module.exports = {
   normalizeEntityName: function () {
   },
   afterInstall: function () {
-    return this.addPackageToProject(
-          {name: 'velocity-animate', target: '1.2.3'}
-    );
+    return this.addPackageToProject('velocity-animate');
   }
 };
