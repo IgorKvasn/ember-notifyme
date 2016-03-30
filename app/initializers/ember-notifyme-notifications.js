6d709f8affbb4b2a07f1b5f9e2752cc516e366d4
Igor Kvasnička <igor.kvasnicka@annotation.sk>
2016-03-30 13:51:43 +0200

    ember initializer deprecation fix

--- app/initializers/ember-notifyme-notifications.js
+++ app/initializers/ember-notifyme-notifications.js
@@ -3,7 +3,7 @@ import NotificationService from 'ember-notifyme/services/notification-service';
 export default {
     name: 'ember-notifyme',
 
-    initialize(registry, application) {
+    initialize(application) {
         application.register('notification:message-service', NotificationService);
 
         ['controller', 'component', 'route', 'router', 'service'].forEach(injectionTarget => {
