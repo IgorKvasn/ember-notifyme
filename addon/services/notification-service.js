80e43413c2588353e99b0a8a163595ede7cd30f8
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2017-09-05 21:56:10 +0200

    remove all except some

--- addon/services/notification-service.js
+++ addon/services/notification-service.js
@@ -4,116 +4,126 @@ import configuration from '../configuration';
 
 export default Ember.Service.extend({
 
-  messages: Ember.A([]),
+	messages: Ember.A([]),
 
-  _getPropertyOrDefault(options, propertyName){
-    //first try to obtain property value from per-message definition
-    let value = options[propertyName];
+	_getPropertyOrDefault(options, propertyName) {
+		//first try to obtain property value from per-message definition
+		let value = options[propertyName];
 
-    //if not available, use global (default) settings
-    if (Ember.isNone(value)){
-      value = configuration.getDefaultSettingForProperty(options.type, propertyName);
-    }
+		//if not available, use global (default) settings
+		if (Ember.isNone(value)) {
+			value = configuration.getDefaultSettingForProperty(options.type, propertyName);
+		}
 
-    return value;
-  },
+		return value;
+	},
 
-  addMessage(options){
+	addMessage(options) {
 
-    //first make sure, message has "some" type
-     options.type = options.type || 'info';
+		//first make sure, message has "some" type
+		options.type = options.type || 'info';
 
-    let timeout = this._getPropertyOrDefault(options, 'timeout');
-    let sticky = this._getPropertyOrDefault(options, 'sticky');
-    if (timeout < 0){
-      sticky = true;
-    }
+		let timeout = this._getPropertyOrDefault(options, 'timeout');
+		let sticky = this._getPropertyOrDefault(options, 'sticky');
+		if (timeout < 0) {
+			sticky = true;
+		}
 
-    let messageObject = NotificationMessage.create({
-           message: options.message,
-           type: options.type,
-           timeout: timeout,
-           sticky,
-           onClick: options.onClick || function(){},
-           onClose: options.onClose || function(){},
-           htmlContent: options.htmlContent,
-           onCloseTimeout: options.onCloseTimeout || function(){},
-           closeOnClick: options.closeOnClick,
-           icon: options.icon,
-           id: options.id
-       });
+		let messageObject = NotificationMessage.create({
+			message: options.message,
+			type: options.type,
+			timeout: timeout,
+			sticky,
+			onClick: options.onClick || function() {},
+			onClose: options.onClose || function() {},
+			htmlContent: options.htmlContent,
+			onCloseTimeout: options.onCloseTimeout || function() {},
+			closeOnClick: options.closeOnClick,
+			icon: options.icon,
+			id: options.id
+		});
 
-    this.get('messages').addObject(messageObject);
+		this.get('messages').addObject(messageObject);
 
-    if (!this.get('message.sticky')){
-      this.startMessageTimer(messageObject);
-    }
-  },
+		if (!this.get('message.sticky')) {
+			this.startMessageTimer(messageObject);
+		}
+	},
 
-  error(message, options){
-    options = options || {};
-    options.type = 'error';
-    options.message = message;
-    this.addMessage(options);
-  },
+	error(message, options) {
+		options = options || {};
+		options.type = 'error';
+		options.message = message;
+		this.addMessage(options);
+	},
 
-  info(message, options){
-    options = options || {};
-    options.message = message;
-    options.type = 'info';
-    this.addMessage(options);
-  },
+	info(message, options) {
+		options = options || {};
+		options.message = message;
+		options.type = 'info';
+		this.addMessage(options);
+	},
 
-  success(message, options){
-    options = options || {};
-    options.message = message;
-    options.type = 'success';
-    this.addMessage(options);
-  },
+	success(message, options) {
+		options = options || {};
+		options.message = message;
+		options.type = 'success';
+		this.addMessage(options);
+	},
 
-  removeMessageFromList(message){
-     this.get('messages').removeObject(message);
-  },
+	removeMessageFromList(message) {
+		this.get('messages').removeObject(message);
+	},
 
-  removeMessage(message){
-      message.set('removeMe', true);
-      Ember.run.cancel(message.get('closeTimer'));
-  },
+	removeMessage(message) {
+		message.set('removeMe', true);
+		Ember.run.cancel(message.get('closeTimer'));
+	},
 
-  removeAll(exceptIdsArray){
-    this.get('messages').forEach((msg)=>{
-      if (Ember.isNone(exceptIdsArray) || !exceptIdsArray.includes(msg.id)){
-        this.removeMessage(msg);
-      }
-    });
-  },
+	removeMessageById(messageId) {
+		let message = this.get('messages').find((msg) => {
+			return msg.get('id') === messageId;
+		});
 
-  startMessageTimer(message){
-    if (message.get('sticky')){
-      return;
-    }
-    message.set('startTimeoutTime', Date.now());
+		if (Ember.isPresent(message)) {
+			this.removeMessage(message);
+		}
+	},
 
-    let timer = Ember.run.later(this, () => {
-        this.removeMessage(message);
+	removeAll(exceptIdsArray) {
+		this.get('messages').forEach((msg) => {
+			if (Ember.isNone(exceptIdsArray) || !exceptIdsArray.includes(msg.id)) {
+				this.removeMessage(msg);
+			}
+		});
+	},
 
-        let onCloseTimeout =  message.get('onCloseTimeout');
-        onCloseTimeout(message);
-    }, message.get('timeout'));
+	startMessageTimer(message) {
+		if (message.get('sticky')) {
+			return;
+		}
+		message.set('startTimeoutTime', Date.now());
 
-    message.set('closeTimer', timer);
-  },
+		let timer = Ember.run.later(this, () => {
+			this.removeMessage(message);
 
-  pauseMessageTimeout(message){
-    if (message.get('sticky')){
-      return;
-    }
+			let onCloseTimeout = message.get('onCloseTimeout');
+			onCloseTimeout(message);
+		}, message.get('timeout'));
 
-    Ember.run.cancel(message.get('closeTimer'));
+		message.set('closeTimer', timer);
+	},
 
-    let remainingTimeout = message.get('timeout') - (Date.now() - message.get('startTimeoutTime'));
-    message.set('timeout', remainingTimeout);
-  }
+	pauseMessageTimeout(message) {
+		if (message.get('sticky')) {
+			return;
+		}
+
+		Ember.run.cancel(message.get('closeTimer'));
+
+		let remainingTimeout = message.get('timeout') - (Date.now() - message.get('startTimeoutTime'));
+		message.set('timeout', remainingTimeout);
+	}
 
 
 
